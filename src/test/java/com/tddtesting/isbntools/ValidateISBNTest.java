b5e89985883c89f2dae94c3e1f9d66cfce60b199
mthsps <dmatheusps@gmail.com>
2022-05-25 21:47:47 -0300

    3. Writing First Tests

---
+++ src/test/java/com/tddtesting/isbntools/ValidateISBNTest.java
@@ -0,0 +1,24 @@
+package com.tddtesting.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateISBNTest {
+
+    @Test
+    public void checkValidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("first value", result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void checkAnInValidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+}
