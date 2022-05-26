017ae817eb94f1e97874b52a8e85b22332c815b2
mthsps <dmatheusps@gmail.com>
2022-05-25 23:00:17 -0300

    4. Practicing the basics

--- src/test/java/com/tddtesting/isbntools/ValidateISBNTest.java
+++ src/test/java/com/tddtesting/isbntools/ValidateISBNTest.java
@@ -7,18 +7,52 @@ import static org.junit.Assert.*;
 public class ValidateISBNTest {
 
     @Test
-    public void checkValidISBN() {
+    public void checkAValid10DigitISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449116");
         assertTrue("first value", result);
-        result = validator.checkISBN("0140177396");
+        result = validator.checkISBN("9781853267338");
         assertTrue("second value", result);
     }
 
     @Test
-    public void checkAnInValidISBN() {
+    public void checkAValid13DigitsISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9783507697706");
+        assertTrue("first value", result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("second value", result);
+    }
+    @Test
+    public void ISBNNumbersEndingInAXAreValid() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("012000030X");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkAnInvalid10DigitISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
+    }
+
+    @Test
+    public void checkAnInvalid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9783507697700");
+        assertFalse(result);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nineDigitISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        validator.checkISBN("123456789");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void NonNumericISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        validator.checkISBN("hellojunit");
     }
 }
